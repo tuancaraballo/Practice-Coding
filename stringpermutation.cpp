08b48df83a4d6b3237645dc4df32677b9027845b
Tuan Anh Tran Caraballo <tuan2606@myth3.Stanford.EDU>
2016-02-19 19:04:00 -0800

    added my first file to github

---
+++ stringpermutation.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <string>
+#include <string.h>
+
+using namespace std;
+
+const int WORD_LENGTH = 100;
+
+
+
+
+
+bool isPermutation (char original [], char permutation []){
+    int len1 = strlen(original);
+    int len2 = strlen(permutation);
+ 
+    if (len1 == len2) {
+        for (int i = 0; i<len2; i++) {
+            char * ptr = strchr(original, permutation[i]);
+            if (ptr == NULL) {
+                return false;
+            }
+        }
+        return true;
+    }
+    return false;
+}
+
+
+int main()
+{
+   
+    char original[WORD_LENGTH];
+    char permutation[WORD_LENGTH];
+    cout << "Enter original word!: ";
+    cin.get(original,WORD_LENGTH);
+    cin.ignore(WORD_LENGTH, '\n');
+    cout<<endl<< "Enter the permutation word: ";
+    cin.get(permutation,WORD_LENGTH);
+    cin.ignore(WORD_LENGTH, '\n');
+    cout<<endl;
+    if (isPermutation (original,permutation)) {
+        cout<<"Yes!"<<endl;
+    } else{
+        cout<<"Nope!"<<endl;
+    }
+    cout<<endl;
+    return 0;
+}
